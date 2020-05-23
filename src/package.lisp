4978b9e46304609ea9f64a2f1c37c50af0c25902
Selwyn Simsek <sgs16@ic.ac.uk>
2020-05-23 10:46:36 +0100

    Add trial-vr.user package for ease of experimentation at the REPL

--- src/package.lisp
+++ src/package.lisp
@@ -1,5 +1,5 @@
-(defpackage :org.shirakumo.fraf.trial.vr
-  (:use :cl)
+(defpackage #:org.shirakumo.fraf.trial.vr
+  (:use #:cl)
   (:local-nicknames (#:vr #:3b-openvr)
 		    (#:trial #:org.shirakumo.fraf.trial)
                     (#:trial-assets #:org.shirakumo.fraf.trial.assets)
@@ -7,10 +7,20 @@
 		    (#:trial-alloy #:org.shirakumo.fraf.trial.alloy)))
 
 #+windows
-(defpackage :org.shirakumo.fraf.trial.vr.windows
+(defpackage #:org.shirakumo.fraf.trial.vr.windows
   (:use #:cl)
   (:local-nicknames (#:com-on #:org.shirakumo.com-on)
                     (#:com-on.cffi #:org.shirakumo.com-on.cffi)
                     (#:trial-vr #:org.shirakumo.fraf.trial.vr)))
 
 
+(defpackage #:org.shirakumo.fraf.trial.vr.user
+  (:use #:cl #:org.shirakumo.fraf.trial.vr #+windows #:org.shirakumo.fraf.trial.vr.windows)
+  (:local-nicknames (#:com-on #:org.shirakumo.com-on)
+                    (#:com-on.cffi #:org.shirakumo.com-on.cffi)
+                    (#:vr #:3b-openvr)
+		    (#:trial #:org.shirakumo.fraf.trial)
+                    (#:trial-assets #:org.shirakumo.fraf.trial.assets)
+		    (#:alloy #:org.shirakumo.alloy)
+		    (#:trial-alloy #:org.shirakumo.fraf.trial.alloy))
+  (:nicknames #:trial-vr.user)) ; naughty! not package local
